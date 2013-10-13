3d15dcf0c9b0174d5e5ef76ed7259f4a3fa7f3bd
Bo Thomsen <boh1996@gmail.com>
2013-10-13 17:58:33 +0200

    GItignore and added a missing file

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from lectioapi import asignments
 import requests
 import datetime
+import config
 
+assignmentList = asignments.assignments(config)
+
+if len(assignmentList) > 0:
+    print "Success"
+else:
+    print "Error"
