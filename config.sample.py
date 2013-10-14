1cae30dd07541a4316769b58848d8a1de83ba702
Bo Thomsen <boh1996@gmail.com>
2013-10-14 14:54:49 +0200

    Refractoring

---
+++ config.sample.py
@@ -0,0 +1,12 @@
+# The standard lectio URL
+lectio_base_url = "https://www.lectio.dk/lectio/"
+# The database system string, mysql+mysqldb etc
+database = ""
+# The database password
+db_password     = ""
+# The database host, 127.0.0.1 etc
+db_host = ""
+# The schema name
+db_database_name = ""
+# The database username
+db_user     = ""
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from lectioapi import asignments
+from lectioapi import assignments
 import requests
 from datetime import *
 from pytz import timezone
@@ -15,9 +15,11 @@ class dic2class(dict):
         for key,val in dic.items():
             self.__dict__[key]=self[key]=dic2class(val) if isinstance(val,dict) else val
 
+# Creates the calendar event title
 def createTitle(event):
     return event["title"]
 
+# Compares the google calendar event with the event from lectio, using startdate and the lectio link
 def sameEvent(googleEvent,localEvent):
     timezone("Europe/Copenhagen")
     startTuple = localEvent["date"].utctimetuple()
@@ -25,18 +27,17 @@ def sameEvent(googleEvent,localEvent):
 
     return localEvent["link"] == googleEvent["description"] and calendar.timegm(googleStartTuple) == calendar.timegm(startTuple)
 
-__author__ = "Bo Thomsen <bo@illution.dk>"
-
 # Crete the database Engine
-engine = create_engine(appConfig.database+'://'+appConfig.db_user+':'+appConfig.db_password+'@'+appConfig.db_host+'/'+appConfig.db_database_name)
+engine = create_engine('%s://%s:%s@%s/%s' % (appConfig.database, appConfig.db_user, appConfig.db_password, appConfig.db_host, appConfig.db_database_name))
 
+# Create a Session
 Session = sessionmaker(bind=engine)
-
-# create a Session
 session = Session()
 
+# Fetches the tasks to run, from the tasks database
 tasks = session.execute("SELECT * FROM assignment_tasks")
 
+# Loops over the tasks
 for task in tasks:
     settings = {
         # App Settings
@@ -56,25 +57,34 @@ for task in tasks:
         "calendar_id" : task["calendar_id"]
     }
 
-    assignmentList = asignments.assignments(dic2class(settings))
+    # Retrieve the assignments from lectio, using the lectio-api library
+    assignmentList = assignments.assignments(dic2class(settings))
 
+    # If assignments is found
     if len(assignmentList) > 0:
-        tokenQuery = session.execute('SELECT * FROM user WHERE user_id="'+task["google_id"]+'"')
+
+        # Retrive the Google Auth information from the table
+        tokenQuery = session.execute('SELECT * FROM user WHERE user_id="%s"' % (task["google_id"]))
 
         GoogleOAuth = google_oauth.GoogleOAuth()
 
+        # Fetch the refresh token, from the database result
         for row in tokenQuery:
             refreshToken = row["refresh_token"]
+            # Fetch the access token from Google using the python-google-oauth library
             accessTokenData = GoogleOAuth.refresh(refreshToken)
             accessToken = accessTokenData.access_token
 
+        # Assign the access token to the Google Calendar library
         GoogleCalendar = GoogleCalendarObject.GoogleCalendar()
         GoogleCalendar.access_token = accessToken
 
+        # Retrieve the existing events from Google Calendar
         googleEvents = GoogleCalendar.events(task["calendar_id"], {
             "timeZone" : "Europe/Copenhagen"
         })
 
+        # If the item key doesn't exist, an error has occurred, proceed to the next task
         if not "items" in googleEvents:
             continue
 
@@ -83,10 +93,12 @@ for task in tasks:
             localEvent["endDate"] = localEvent["date"] + timedelta(minutes = 10)
             found = False
 
+            # Loop through the Google events to find the local event
             for googleEvent in googleEvents["items"]:
                 if sameEvent(googleEvent,localEvent):
                     found = True
 
+            # If it doesn't exist, create it
             if found == False:
                 GoogleCalendar.insertEvent(task["calendar_id"],{
                     "start" : {"timeZone" : "Europe/Copenhagen","dateTime" : localEvent["date"].strftime('%Y-%m-%dT%H:%M:%S.000')},
@@ -100,10 +112,13 @@ for task in tasks:
         # Sync Google -> Local
         for googleEvent in googleEvents["items"]:
             found = False
+
+            # Loop through the local events to find the Google event
             for localEvent in assignmentList:
                 if sameEvent(googleEvent, localEvent):
                     found = True
 
+            # If it doesn't exist there, delete it from Google Calendar
             if found == False:
                 print "Delete"
                 GoogleCalendar.deleteEvent(task["calendar_id"], googleEvent["id"])
